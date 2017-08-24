e62bb99d18dea8b305350d72b184b329caec65b3
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-08-24 22:08:23 +0300

    I have added the property for when the client was added to the system and the corresponding test passed

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,9 +1,17 @@
+import java.time.LocalDateTime;
+
+
 public class Client {
 private String mName;
+private LocalDateTime mCreatedAt;
 public Client(String name){
         mName=name;
+        mCreatedAt=LocalDateTime.now();
 }
 public String getName(){
         return mName;
+}
+public LocalDateTime isCreatedAt(){
+        return mCreatedAt;
 }
 }
--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -3,17 +3,25 @@
  */
 import org.junit.Test;
 import static org.junit.Assert.*;
+import java.time.LocalDateTime;
 
 public class ClientTest{
+  //test for instance
   @Test
   public void instantiateswell_true(){
     Client newClient=new Client("leah");
     assertEquals(true, newClient instanceof Client);
   }
-
+ //test if it instantiates with property of name
   @Test
   public void Client_instantiatesWithName_string(){
     Client newClient=new Client("leah");
     assertEquals("leah",newClient.getName());
+  }
+ //get time when client was registered
+  @Test
+  public void getCreatedAt_instantiatesWithCurrentTime_today(){
+      Client newClient=new Client("leah");
+      assertEquals(LocalDateTime.now().getDayOfWeek(), newClient. isCreatedAt().getDayOfWeek());
   }
 }
