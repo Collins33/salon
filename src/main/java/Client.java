a651ef24b4947ecc0f2e9bb905c3657dc715bcc0
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-08-25 15:45:58 +0300

    I have intergrated database to the Client class

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,36 +1,79 @@
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-
+import org.sql2o.*;
 
 
 public class Client {
-private String mName;
-private LocalDateTime mCreatedAt;
-private static List<Client> instances=new ArrayList<Client>();
-private int mId;
+private String name;
+private LocalDateTime createdAt;
+
+private int id;
+
+
+//constructor
 public Client(String name){
-        mName=name;
-        mCreatedAt=LocalDateTime.now();
-        instances.add(this);
-        mId=instances.size();
+        this.name=name;
+        createdAt=LocalDateTime.now();
 }
+
+
+//getter methods
 public String getName(){
-        return mName;
+        return name;
 }
 public LocalDateTime isCreatedAt(){
-        return mCreatedAt;
-}
-public static List<Client> all(){
-        return instances;
-}
-public static void clear(){
-        instances.clear();
+        return createdAt;
 }
 public int getId(){
-  return mId;
+  return id;
 }
 public static Client find(int id) {
-  return instances.get(id - 1);
+  try(Connection con=DB.sql2o.open()){
+    String sql="SELECT * FROM clients where id=:id";
+    Client client=con.createQuery(sql)
+    .addParameter("id",id)
+    .executeAndFetchFirst(Client.class);
+    return client;
+  }
+
 }
+
+
+//retrieves from the database under clients table
+//retrieves into ArrayList
+//all() returns the ArrayList created containing the name and id
+public static List<Client> all(){
+String sql="SELECT id,name FROM clients";
+try(Connection con=DB.sql2o.open()){
+ return con.createQuery(sql).executeAndFetch(Client.class);
+
+}
+}
+//override the equals()
+@Override
+public boolean equals(Object myClient){
+  if(!(myClient instanceof Client)){
+    return false;
+  }
+  else{
+    Client firstClient=(Client) myClient;
+    return this.getName().equals(firstClient.getName())&&
+    this.getId() == firstClient.getId();
+
+  }
+}
+//saves into the database
+public void save(){
+  try(Connection con=DB.sql2o.open()){
+    String sql="INSERT INTO clients (name) VALUES (:name)";
+    this.id=(int)con.createQuery(sql,true)
+    .addParameter("name",this.name)
+    .executeUpdate()
+    .getKey();
+  }
+}
+
+
+
 }
