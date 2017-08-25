b2cb47eacbeae605ce0a11237c37e975f4df74f9
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-08-25 11:58:49 +0300

    I have added the route to display the listed stylists and the corresponding template

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,19 +8,19 @@ public class App {
 public static void main(String[] args) {
         staticFileLocation("/public");
         String layout = "templates/layout.vtl";
-
+        //display the homepage
         get("/",(request,response) ->{
                     Map<String, Object> model = new HashMap<String, Object>();
                     model.put("template","templates/index.vtl");
                     return new ModelAndView(model,layout);
             },new VelocityTemplateEngine());
-
+        //display the stylist form
         get("/stylist/new",(request,response) ->{
                     Map<String, Object> model = new HashMap<String, Object>();
                     model.put("template","templates/stylist-form.vtl");
                     return new ModelAndView(model,layout);
             },new VelocityTemplateEngine());
-
+        //process the stylist form
         post("/stylist",(request,response) ->{
                      Map<String, Object> model = new HashMap<String, Object>();
                      String name =request.queryParams("name");
@@ -28,5 +28,12 @@ public static void main(String[] args) {
                      model.put("template","templates/awesome.vtl");
                      return new ModelAndView(model,layout);
              },new VelocityTemplateEngine());
+
+        get("/stylist", (request,response)->{
+                    Map<String, Object> model = new HashMap<String, Object>();
+                    model.put("stylists",Stylist.all());
+                    model.put("template","templates/stylists.vtl");
+                    return new ModelAndView(model,layout);
+            },new VelocityTemplateEngine());
 }
 }
