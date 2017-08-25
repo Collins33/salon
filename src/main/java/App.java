71dbf3d073c9bf3cf4e4878b41ded9aa05e0b96d
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-08-25 11:45:54 +0300

    I have added the route that will process the stylist form and display a page

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -20,5 +20,13 @@ public static void main(String[] args) {
                     model.put("template","templates/stylist-form.vtl");
                     return new ModelAndView(model,layout);
             },new VelocityTemplateEngine());
+
+        post("/stylist",(request,response) ->{
+                     Map<String, Object> model = new HashMap<String, Object>();
+                     String name =request.queryParams("name");
+                     Stylist newStylist=new Stylist(name);
+                     model.put("template","templates/awesome.vtl");
+                     return new ModelAndView(model,layout);
+             },new VelocityTemplateEngine());
 }
 }
