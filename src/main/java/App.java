7e2af8772d809a5bc1525de82e8795e366afdd55
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-08-25 12:11:18 +0300

    I have added the dynamic route to show more details about the clicked stylist using the id

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -28,11 +28,20 @@ public static void main(String[] args) {
                      model.put("template","templates/awesome.vtl");
                      return new ModelAndView(model,layout);
              },new VelocityTemplateEngine());
-
+        //display list of stylists
         get("/stylist", (request,response)->{
                     Map<String, Object> model = new HashMap<String, Object>();
                     model.put("stylists",Stylist.all());
                     model.put("template","templates/stylists.vtl");
+                    return new ModelAndView(model,layout);
+            },new VelocityTemplateEngine());
+
+        //display more info about the stylist
+        get("/stylist/:id", (request,response) ->{
+                    Map<String, Object> model = new HashMap<String, Object>();
+                    Stylist stylist=Stylist.find(Integer.parseInt(request.params(":id")));
+                    model.put("stylist",stylist);
+                    model.put("template","templates/stylist.vtl");
                     return new ModelAndView(model,layout);
             },new VelocityTemplateEngine());
 }
