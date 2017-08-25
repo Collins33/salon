a82d66fbce971cdd92380c7edf1d4759e024b9c9
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-08-25 11:37:28 +0300

    I have added the route to move to where the stylist form is and added the template to display the form

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,9 +10,15 @@ public static void main(String[] args) {
         String layout = "templates/layout.vtl";
 
         get("/",(request,response) ->{
-          Map<String, Object> model = new HashMap<String, Object>();
-          model.put("template","templates/index.vtl");
-          return new ModelAndView(model,layout);
-        },new VelocityTemplateEngine());
+                    Map<String, Object> model = new HashMap<String, Object>();
+                    model.put("template","templates/index.vtl");
+                    return new ModelAndView(model,layout);
+            },new VelocityTemplateEngine());
+
+        get("/stylist/new",(request,response) ->{
+                    Map<String, Object> model = new HashMap<String, Object>();
+                    model.put("template","templates/stylist-form.vtl");
+                    return new ModelAndView(model,layout);
+            },new VelocityTemplateEngine());
 }
 }
